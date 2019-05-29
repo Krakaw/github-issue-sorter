7453c515237cc43c9d4dc82efa85169b4064e747
Krakaw <41575888+Krakaw@users.noreply.github.com>
2019-05-29 14:53:31 +0200

    Build

---
+++ precache-manifest.96d836cbfe362896456fef350ccd14e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee7d3ef79af49e2c215d5bd6fa6946f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a826759d30a57caeeb98",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "faacac8ebf10ddb7d5c7",
+    "url": "/static/js/2.66316d18.chunk.js"
+  },
+  {
+    "revision": "a826759d30a57caeeb98",
+    "url": "/static/js/main.c8f83e04.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
