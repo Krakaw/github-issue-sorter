c614d00715ce7669ac38158d9559b4cadbfedf55
Krakaw <41575888+Krakaw@users.noreply.github.com>
2019-05-29 14:56:33 +0200

    Build

---
+++ precache-manifest.b29f7a20b1287d08f67e844f5beca95d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a58f7b982fcd59477e49da26520d02d",
+    "url": "/github-issue-sorter/index.html"
+  },
+  {
+    "revision": "a98fe7edae0b80c9feb9",
+    "url": "/github-issue-sorter/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "faacac8ebf10ddb7d5c7",
+    "url": "/github-issue-sorter/static/js/2.66316d18.chunk.js"
+  },
+  {
+    "revision": "a98fe7edae0b80c9feb9",
+    "url": "/github-issue-sorter/static/js/main.3c314e14.chunk.js"
+  },
+  {
+    "revision": "0d5f8108b970779cc163",
+    "url": "/github-issue-sorter/static/js/runtime~main.56d43756.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.96d836cbfe362896456fef350ccd14e6.js"
+  "/github-issue-sorter/precache-manifest.b29f7a20b1287d08f67e844f5beca95d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github-issue-sorter/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
